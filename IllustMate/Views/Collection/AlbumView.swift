b51108f2a73728b385a75f24d7ed7048431cacdb
katagaki <68231035+katagaki@users.noreply.github.com>
2023-10-06 11:36:41 +0800

    Fix warnings

--- IllustMate/Views/Collection/AlbumView.swift
+++ IllustMate/Views/Collection/AlbumView.swift
@@ -79,12 +79,13 @@ struct AlbumView: View {
                                 .shadow(color: .black.opacity(0.2), radius: 4.0, x: 0.0, y: 4.0)
                                 .gesture(
                                     DragGesture()
-                                        .updating($illustrationDisplayOffset) { value, state, transaction in
+                                        .updating($illustrationDisplayOffset) { value, state, _ in
                                             state = value.translation
                                         }
                                         .onEnded { gesture in
-                                            let hypotenuse = sqrt((gesture.translation.width * gesture.translation.width) +
-                                                                  (gesture.translation.height * gesture.translation.height))
+                                            let width = gesture.translation.width
+                                            let height = gesture.translation.height
+                                            let hypotenuse = sqrt((width * width) + (height * height))
                                             if hypotenuse > 50.0 {
                                                 withAnimation(.snappy.speed(2)) {
                                                     self.displayedIllustration = nil
