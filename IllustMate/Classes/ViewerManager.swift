e9ca4a8be549514a4e17a003cf5695ba61650006
katagaki <68231035+katagaki@users.noreply.github.com>
2023-10-16 13:51:13 +0800

    Implement image cache to prevent memory leak

--- IllustMate/Classes/ViewerManager.swift
+++ IllustMate/Classes/ViewerManager.swift
@@ -14,25 +14,32 @@ class ViewerManager {
     var displayedIllustration: Illustration?
     var displayedImage: UIImage?
 
+    var imageCache: [String: UIImage] = [:]
+
     func setDisplay(_ illustration: Illustration) {
-        // TODO: Fix memory leak here
         displayedIllustration = illustration
         do {
-            if let data = try? Data(contentsOf: URL(filePath: illustration.illustrationPath())),
-               let image = UIImage(data: data) {
+            if let image = imageCache[illustration.id] {
                 displayedImage = image
             } else {
-                try FileManager.default.startDownloadingUbiquitousItem(
-                    at: URL(filePath: illustration.illustrationPath()))
-                var isDownloaded: Bool = false
-                while !isDownloaded {
-                    if FileManager.default.fileExists(atPath: illustration.illustrationPath()) {
-                        isDownloaded = true
-                    }
-                }
-                let data = try? Data(contentsOf: URL(filePath: illustration.illustrationPath()))
-                if let data, let image = UIImage(data: data) {
+                if let data = try? Data(contentsOf: URL(filePath: illustration.illustrationPath())),
+                   let image = UIImage(data: data) {
                     displayedImage = image
+                    imageCache[illustration.id] = image
+                } else {
+                    try FileManager.default.startDownloadingUbiquitousItem(
+                        at: URL(filePath: illustration.illustrationPath()))
+                    var isDownloaded: Bool = false
+                    while !isDownloaded {
+                        if FileManager.default.fileExists(atPath: illustration.illustrationPath()) {
+                            isDownloaded = true
+                        }
+                    }
+                    let data = try? Data(contentsOf: URL(filePath: illustration.illustrationPath()))
+                    if let data, let image = UIImage(data: data) {
+                        displayedImage = image
+                        imageCache[illustration.id] = image
+                    }
                 }
             }
         } catch {
