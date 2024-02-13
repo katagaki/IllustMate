20654f45e5948df00c1f8f0448ee1d9a2b89fb23
katagaki <68231035+katagaki@users.noreply.github.com>
2024-02-13 22:24:05 +0900

    Show actual photos in album cover

--- IllustMate/Views/Collection/AlbumGridLabel.swift
+++ IllustMate/Views/Collection/AlbumGridLabel.swift
@@ -12,10 +12,20 @@ struct AlbumGridLabel: View {
     var namespace: Namespace.ID
 
     var album: Album
+    var representativePhotos: [Image]
+
+    init(namespace: Namespace.ID, album: Album) {
+        self.namespace = namespace
+        self.album = album
+        self.representativePhotos = AlbumGridLabel.representativePhotosFor(album: album)
+    }
 
     var body: some View {
         VStack(alignment: .leading, spacing: 8.0) {
-            AlbumCover(cornerRadius: 8.0, shadowSize: 4.0, data: album.coverPhoto)
+            AlbumCover(cornerRadius: 8.0, shadowSize: 4.0, data: album.coverPhoto,
+                       primaryImage: representativePhotos[0],
+                       secondaryImage: representativePhotos[1],
+                       tertiaryImage: representativePhotos[2])
                 .toggledMatchedGeometryEffect(id: "\(album.id).Image", in: namespace)
             VStack(alignment: .leading, spacing: 2.0) {
                 Text(album.name)
@@ -45,5 +55,43 @@ struct AlbumGridLabel: View {
             }
         }
         .contentShape(.rect)
+    }
+
+    static func representativePhotosFor(album: Album) -> [Image] {
+        var imagesToReturn: [Image] = []
+        let genericImage: Image = Image(uiImage: UIImage(named: "Album.Generic")!)
+        if let illustrations = album.childIllustrations {
+            let sortedIllustrations = illustrations.sorted { lhs, rhs in
+                lhs.dateAdded < rhs.dateAdded
+            }
+            let primaryImage: Illustration? = sortedIllustrations.count >= 1 ? sortedIllustrations[0] : nil
+            let secondaryImage: Illustration? = sortedIllustrations.count >= 2 ? sortedIllustrations[1] : nil
+            let tertiaryImage: Illustration? = sortedIllustrations.count >= 3 ? sortedIllustrations[2] : nil
+            if let coverPhoto = album.coverPhoto, let coverImage = UIImage(data: coverPhoto) {
+                imagesToReturn.append(Image(uiImage: coverImage))
+                if let primaryImage, let thumbnail = primaryImage.cachedThumbnail?.image() {
+                    imagesToReturn.append(Image(uiImage: thumbnail))
+                }
+                if let secondaryImage, let thumbnail = secondaryImage.cachedThumbnail?.image() {
+                    imagesToReturn.append(Image(uiImage: thumbnail))
+                }
+            } else {
+                if let primaryImage, let thumbnail = primaryImage.cachedThumbnail?.image() {
+                    imagesToReturn.append(Image(uiImage: thumbnail))
+                }
+                if let secondaryImage, let thumbnail = secondaryImage.cachedThumbnail?.image() {
+                    imagesToReturn.append(Image(uiImage: thumbnail))
+                }
+                if let tertiaryImage, let thumbnail = tertiaryImage.cachedThumbnail?.image() {
+                    imagesToReturn.append(Image(uiImage: thumbnail))
+                }
+            }
+        }
+        if imagesToReturn.count < 3 {
+            for _ in imagesToReturn.count...3 {
+                imagesToReturn.append(genericImage)
+            }
+        }
+        return imagesToReturn
     }
 }
--- IllustMate/Views/Shared/AlbumCover.swift
+++ IllustMate/Views/Shared/AlbumCover.swift
@@ -10,29 +10,112 @@ import SwiftUI
 struct AlbumCover: View {
 
     var length: CGFloat?
-    var cornerRadius: Double = 6.0
-    var shadowSize: Double = 2.0
+    var cornerRadius: Double
+    var shadowSize: Double
     var data: Data?
 
-    @State var coverPhoto: Image?
-    @State var secondaryImage: Image?
-    @State var tertiaryImgae: Image?
+    @State var primaryImage: Image
+    @State var secondaryImage: Image
+    @State var tertiaryImage: Image
+
+    init(length: CGFloat? = nil, cornerRadius: Double = 6.0, shadowSize: Double = 2.0,
+         data: Data? = nil, primaryImage: Image? = nil, secondaryImage: Image? = nil, tertiaryImage: Image? = nil) {
+        self.length = length
+        self.cornerRadius = cornerRadius
+        self.shadowSize = shadowSize
+        self.data = data
+        if let primaryImage {
+            self.primaryImage = primaryImage
+        } else {
+            self.primaryImage = Image(.albumGeneric)
+        }
+        if let secondaryImage {
+            self.secondaryImage = secondaryImage
+        } else {
+            self.secondaryImage = Image(.albumGeneric)
+        }
+        if let tertiaryImage {
+            self.tertiaryImage = tertiaryImage
+        } else {
+            self.tertiaryImage = Image(.albumGeneric)
+        }
+    }
 
     var body: some View {
         ZStack(alignment: .center) {
-            if let coverPhoto {
-                AlbumFolderCover(image1: coverPhoto, image2: secondaryImage, image3: tertiaryImgae)
-                    .toggledTransition(.opacity.animation(.snappy.speed(2)))
-            } else {
-                Image("Album.Generic")
-                    .resizable()
+            GeometryReader { metrics in
+                ZStack {
+                    RoundedRectangle(cornerRadius: metrics.size.height * 0.12, style: .continuous)
+                        .fill(LinearGradient(gradient: Gradient(colors: [.orange, .yellow]),
+                                             startPoint: .topLeading,
+                                             endPoint: .bottomTrailing
+                                            ))
+                        .frame(width: metrics.size.width, height: metrics.size.height)
+                        .shadow(color: .black.opacity(0.2), radius: 4.0, x: 0.0, y: metrics.size.height * 0.06)
+                    ZStack {
+                        tertiaryImage
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: metrics.size.width * 0.66,
+                                   height: metrics.size.height * 0.66)
+                            .clipShape(RoundedRectangle(cornerRadius: metrics.size.height * 0.05))
+                            .offset(x: -metrics.size.width * 0.13, y: -metrics.size.height * 0.09)
+                            .rotationEffect(.degrees(-10))
+                        secondaryImage
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: metrics.size.width * 0.66,
+                                   height: metrics.size.height * 0.66)
+                            .clipShape(RoundedRectangle(cornerRadius: metrics.size.height * 0.05))
+                            .offset(x: 0.0, y: -metrics.size.height * 0.06)
+                            .rotationEffect(.degrees(0))
+                            .shadow(color: .black.opacity(0.2), radius: 4.0, x: 0.0, y: 4.0)
+                        primaryImage
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: metrics.size.width * 0.66,
+                                   height: metrics.size.height * 0.66)
+                            .clipShape(RoundedRectangle(cornerRadius: metrics.size.height * 0.05))
+                            .offset(x: metrics.size.width * 0.13, y: -metrics.size.height * 0.07)
+                            .rotationEffect(.degrees(10))
+                            .shadow(color: .black.opacity(0.2), radius: 4.0, x: 2.0, y: 2.0)
+                    }
+                    .offset(x: 0, y: -metrics.size.height * 0.1)
+                    UnevenRoundedRectangle(topLeadingRadius: 0.0,
+                                           bottomLeadingRadius: metrics.size.height * 0.12,
+                                           bottomTrailingRadius: metrics.size.height * 0.12,
+                                           topTrailingRadius: 0.0,
+                                           style: .continuous)
+                    .fill(LinearGradient(gradient: Gradient(colors: [.yellow, .orange]),
+                                         startPoint: .topLeading,
+                                         endPoint: .bottomTrailing
+                                        ))
+                    .frame(width: metrics.size.width, height: metrics.size.height * 0.53)
+                    .overlay {
+                        UnevenRoundedRectangle(topLeadingRadius: 0.0,
+                                               bottomLeadingRadius: metrics.size.height * 0.10,
+                                               bottomTrailingRadius: metrics.size.height * 0.10,
+                                               topTrailingRadius: 0.0,
+                                               style: .continuous)
+                        .stroke(LinearGradient(gradient: Gradient(colors: [.black.opacity(0.5), .black.opacity(0.2)]),
+                                               startPoint: .topLeading,
+                                               endPoint: .bottomTrailing
+                                              ),
+                                style: .init(lineWidth: CGFloat(0.8), lineCap: .round, lineJoin: .round,
+                                             dash: [CGFloat(metrics.size.width * 0.04)]))
+                        .frame(width: metrics.size.width * 0.95, height: metrics.size.height * 0.485)
+                    }
+                    .offset(y: metrics.size.height * 0.47 / 2)
+                    .shadow(color: .black.opacity(0.2), radius: 4.0, x: 0.0, y: -metrics.size.height * 0.04)
+                }
             }
+            .toggledTransition(.opacity.animation(.snappy.speed(2)))
         }
         .scaledToFit()
         .frame(width: length, height: length)
         .onAppear {
             if let data, let coverImage = UIImage(data: data) {
-                coverPhoto = Image(uiImage: coverImage)
+                primaryImage = Image(uiImage: coverImage)
             }
         }
     }
